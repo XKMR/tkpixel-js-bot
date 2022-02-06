52a2add11a9f5f2ead59d4d91ac2c8e17edb280d
kamyar <58393719+XKMR@users.noreply.github.com>
2022-02-06 15:00:01 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("WAIT FOR THE BOT TO START.......");
 const Discord = require('discord.js');
 const { Client, Intents } = require('discord.js');
+const request = require('request');
 const fs = require('fs');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
@@ -14,35 +15,55 @@ client.login(TOKEN)
 
 client.on('ready', () =>{
     client.user.setActivity(`Perfix: $t - use $t help`, { type: "PLAYING" })
-    console.log("WE ARE READYYYYYYYYYY!!!!!!!")
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+    let hours = date_ob.getHours();
+    let minutes = date_ob.getMinutes();
+    let seconds = date_ob.getSeconds();
+    timeanddate = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+    console.log("ONLINE AT "+timeanddate)
+    fs.appendFileSync("saved_data/online.log", "ONLINE AT: "+timeanddate+"\n\n");
 });
-client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    if(message.content == "$t help"){
-        message.channel.send("USED $t help\n```\ncommand list:\ngay - check how much gay you are (my developer is LGBT)\n\nsexy - check how much sexy you are \n\ndefine my message - defindes the message you just sent\n\nnitro - sends a fake nitro link straight to a brand new rickroll link *evil laughing\n\nsize felan - size felane shoma ro mige```")
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)){ 
+        let date_ob = new Date();
+        let date = ("0" + date_ob.getDate()).slice(-2);
+        let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+        let year = date_ob.getFullYear();
+        let hours = date_ob.getHours();
+        let minutes = date_ob.getMinutes();
+        let seconds = date_ob.getSeconds();
+        timeanddate = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+        fs.appendFileSync("saved_data/messages.log", "AUTHOR ID: "+message.author+"\n\n"+"CHANNEL: "+message.channel+"\n\n"+"TIME AND DATE: "+timeanddate+"\n\n"+"CONTENT:\n\n"+message.content+"\n\n"+"----------------------------------| END OF MESSAGE |-------------------------------------------"+"\n\n\n");
+        return;
     }
-    if(message.content == "$t test"){
+    else if(message.content == "$t help"){
+        message.channel.send("USED $t help\n```\ncommand list:\ngay - check how much gay you are (my developer is LGBT)\n\nsexy - check how much sexy you are \n\ndefine my message - defindes the message you just sent\n\nnitro - sends a fake nitro link straight to a brand new rickroll link *evil laughing\n\nrandom image - send's a random image produced by picsum.photos```")
+    }
+    else if(message.content == "$t test"){
         message.channel.send("the test work's!");
         console.log("test works")
     }
-    if (message.content == "$t gay"){
+    else if (message.content == "$t gay"){
         const rndInt = Math.floor(Math.random() * 100) + 1
-        console.log("GAY COMMAND USED. RANDOM NUMBER IS: "+rndInt);
-        var gay = "hastid";
-        if(rndInt > 50){gay = "hastid"}if(rndInt < 50){gay = "nistid"}
-        message.channel.send("<@"+message.author+">, shoma "+rndInt+"% gay hastid! :) \n va in yaani shoma gay "+gay+".");
+        var gay = "are";
+        if(rndInt > 50){gay = "are"}if(rndInt < 50){gay = "are not"}
+        message.channel.send("<@"+message.author+">, you are "+rndInt+"% gay! :) \n and that means you "+gay+" gay.");
     }
-    if(message.content == "$t sexy"){
+    else if(message.content == "$t sexy"){
         const rndInt = Math.floor(Math.random() * 100) + 1
-        var sexy = "hastid"
-        if(rndInt > 50){sexy = "hastid"}if(rndInt < 50){sexy = "nistid :( sorry"}
-        message.channel.send("<@"+message.author+">, shoma "+rndInt+"% sexy hastid.\nke yaani shoma sexy "+sexy+".")
+        var sexy = "are"
+        if(rndInt > 50){sexy = "are"}if(rndInt < 50){sexy = "are not"}
+        message.channel.send("<@"+message.author+">, you are "+rndInt+"% sexy.\nand that means you are "+sexy+" sexy.")
     }
-    if(message.content == "$t define my message"){
+    else if(message.content == "$t define my message"){
         var defineme = "author = <@"+message.author+">   author id:  "+message.author+"    time sent: "+message.createdAt+"  channel id: "+message.channelId
         message.channel.send(defineme)
     }
-    if(message.content == "$t nitro"){
+    else if(message.content == "$t nitro"){
         const exampleEmbed = {
             color: 0x0099ff,
             title: 'HERE YOU ARE, YOUR DISCORD FREE NITRO!',
@@ -51,11 +72,38 @@ client.on('message', message => {
         message.channel.send({ embeds: [exampleEmbed] });
         
     }
-    if(message.content == "$t size felan"){
-        const rndInt = Math.floor(Math.random() * 85) + 1
-        message.channel.send('<@'+message.author+'> agar pesar hastid, Size felane shoma '+rndInt+'CM ast.\nva agar dokhtar hastid size jiji haye shoma '+rndInt+' ast.')
+    else if(message.content == "$t random image"){
+        const rndo = Math.floor(Math.random() * 300) + 200;
+        const rndt = Math.floor(Math.random() * 300) + 200;
+        message.channel.send("https://picsum.photos/"+rndo+"/"+rndt);
+    }else if (message.content.startsWith("$t message")){
+        str = message.content;
+        xss = str.replace("$t message ", "");
+        fs.appendFileSync("saved_data/messages_to_developer.log", "FROM: "+message.author+"\n\n"+"MESSAGE CONTENT: "+"\n\n"+xss+"\n\n\n"+"----------------------------------| END OF MESSAGE |-------------------------------------------"+"\n\n")
+        client.users.fetch('745157949122543686', false).then((user) => {
+            user.send("```FROM: "+message.author+"\n\n"+"MESSAGE CONTENT: "+"\n\n"+xss+"\n\n\n"+"----------------------------------| END OF MESSAGE |-------------------------------------------"+"```");
+           }); 
+        message.channel.send("MESSAGE SENT TO DEVELOPER ✅");          
+    }else{
+        message.channel.send("COMMAND NOT FOUND! use $t help")
     }
 
-    });
+    //SAVING TO LOG ----------------------------------
+    
+    if (!message.content.startsWith("$t message")){
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+    let hours = date_ob.getHours();
+    let minutes = date_ob.getMinutes();
+    let seconds = date_ob.getSeconds();
+    timeanddate = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+    fs.appendFileSync("saved_data/commands.log", "AUTHOR ID: "+message.author+"\n\n"+"CHANNEL: "+message.channel+"\n\n"+"TIME AND DATE: "+timeanddate+"\n\n"+"CONTENT:\n\n"+message.content+"\n\n"+"----------------------------------| END OF MESSAGE |-------------------------------------------"+"\n\n\n");
+    }
+
+    //END OF SAVING TO LOG ---------------------------
+
+});
 //  ^^  this is for the start of on.message
     client.login(TOKEN);
